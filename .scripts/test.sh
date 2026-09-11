f40b65b49cfa74e200a2320c37065245c3cb7646
rzatonskiy <roman.zatonskiy@quadcode.com>
2026-09-11 22:17:05 +0300

    ci: run the test suite under the race detector
    * add -race to the test script, so a reintroduced race fails the build
    * raise the go matrix to 1.23-1.27, the module requires go 1.23.0

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -7,7 +7,7 @@ if [[ -f coverage.txt ]]; then
 fi
 
 for d in $(go list ./... | grep -v ditest); do
-  go test -coverprofile=profile.out -coverpkg=./... -covermode=atomic "$d"
+  go test -race -coverprofile=profile.out -coverpkg=./... -covermode=atomic "$d"
   if [[ -f profile.out ]]; then
     cat profile.out >>coverage.txt
     rm profile.out
